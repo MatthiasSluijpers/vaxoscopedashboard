91a634c0075b31d7a64d61eca9da194a3976d1a0
MatthiasSluijpers <m.j.t.m.sluijpers@students.uu.nl>
2021-10-27 22:22:22 +0200

    Added country attitude level comparison with attitude dropdown.

--- apps/comparison.py
+++ apps/comparison.py
@@ -17,18 +17,14 @@ import dash_bootstrap_components as dbc
 # Data vaccinatiegraad COMP
 vaccinations = pd.read_csv("data/vaccinations.csv")
 vacComp = vaccinations.copy()
-vacComp = vacComp[(vacComp["location"] == "Netherlands") | (vacComp["location"] == "United Kingdom") | (vacComp["location"] == "United States")][["location", "date","people_fully_vaccinated_per_hundred"]]
+vacComp = vacComp.loc[vacComp["location"].isin(["Netherlands", "United Kingdom", "United States"])][["location", "date","people_fully_vaccinated_per_hundred"]]
 vacComp.dropna(inplace = True)
 
-# Data attitudes UK
+# Data attitudes COMP
 attitudes = pd.read_csv("data/attitudes.csv")
-attUK = attitudes.copy()
-attUK = attUK[attUK["Entity"] == "United Kingdom"]
-attUK.columns = ["country", "code", "date", "unwilling", "uncertain", "willing", "vaccinated"]
-
-# Data inkomensgroepen UK
-incomeUK = pd.read_csv("data/incomeUK.csv")
-
+attComp = attitudes.copy()
+attComp.columns = ["country", "code", "date", "unwilling", "uncertain", "willing", "vaccinated"]
+attComp = attComp.loc[attComp["country"].isin(["Netherlands", "United Kingdom", "United States"])]
 
 
 ## LAYOUT VAN PAGINA ------------------------------------------------------------------------------------------------
@@ -63,6 +59,26 @@ layout = dbc.Container([
         ], width = {"size":0})
     ]),
 
+    dbc.Row([
+
+        dbc.Col([
+            html.Div([html.P("Attitudes towards vaccination per Country:")],
+                            className='custom-graph-title'),
+            dcc.Dropdown(   id='attitude-dropwdown', multi=False,
+                            options = [{"label": "Unwilling to get vaccinated", "value":"unwilling"},
+                                      {"label": "Uncertain about vaccination", "value":"uncertain"},
+                                      {"label": "Willing but not yet vaccinated", "value":"willing"}],
+                            value = "unwilling"
+                     ),
+            dcc.Graph(id='attitudes_Comp', figure={})
+        ], width = {"size":8, "offset":2}),
+
+        dbc.Col([
+            #dcc.Graph(id='graph_name', figure={})
+        ], width = {"size":0})
+    ]),
+
+
 
 ], fluid = True)
 
@@ -90,3 +106,27 @@ def update_graph(selected_countries):
     figVacComp.update_traces(connectgaps=True)
     figVacComp.add_hline(y=90, line_width=2, line_dash="dash", opacity=0.2, annotation_text="<i>theoretical herd immunity</i>", annotation_position="top right")
     return figVacComp
+
+# Figuur attitudes
+
+@app.callback(
+    Output('attitudes_Comp', 'figure'),
+    [Input('selected-countries', 'value'),
+     Input('attitude-dropwdown', 'value')]
+)
+def update_graph(selected_countries, attitude_dropdown):
+    def custom_legend_name(figure, new_names):
+        for i, new_name in enumerate(new_names):
+            figure.data[i].name = new_name
+    figAttComp = px.line(attComp.loc[attComp['country'].isin(selected_countries)],
+                    x="date",
+                    y= attitude_dropdown,
+                    color="country",
+                    labels = {"date" : "Date",
+                              "value" : "Share of Population (%)"},
+                    range_y = [0,100],
+                    template = "seaborn",
+                    color_discrete_map = {"unwilling":"black", "uncertain":"purple", "willing":"seagreen"})
+    figAttComp.update_traces(connectgaps=True)
+    #custom_legend_name(attComp, ['unwilling to get vaccinated','uncertain about vaccination', "willing but not yet vaccinated"])
+    return figAttComp
